e360708ad68f50dbb83725eb5c7d0e353a9f9de5
hoainam2k1 <hoainam20052001@gmail.com>
2022-07-30 18:42:19 +0700

    Confirm Email register

---
+++ Shop_DT/Service/SendMailService.cs
@@ -0,0 +1,51 @@
+﻿using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using System;
+using System.Threading.Tasks;
+
+namespace Shop_DT.Service
+{
+    public class SendMailService
+    {
+        MailSettings _mailSettings;
+        public SendMailService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+        public async Task<string> SendMail(MailContent mailContent)
+        {
+            var email = new MimeMessage();
+            email.Sender = new MailboxAddress(_mailSettings.DisplayName,_mailSettings.Mail);
+            email.From.Add (new MailboxAddress(_mailSettings.DisplayName,_mailSettings.Mail));
+            email.To.Add(new MailboxAddress(mailContent.To, mailContent.To));
+            email.Subject = mailContent.Subject;
+
+            var builder = new BodyBuilder();
+            builder.HtmlBody = mailContent.Body;
+            email.Body = builder.ToMessageBody();
+
+            using var smtp = new MailKit.Net.Smtp.SmtpClient();
+            try
+            {
+                await smtp.ConnectAsync(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+                await smtp.AuthenticateAsync(_mailSettings.Mail, _mailSettings.Password);
+                await smtp.SendAsync(email);
+                
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "Fail" + ex.Message;
+            }
+            smtp.Disconnect(true);
+            return "Send sucessfully";
+        }
+    }
+    public class MailContent
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
